5d0497826de6ce5153785edb6d32336d54263d16
firebreather <dtenny95@gmail.com>
2022-09-07 04:29:38 +0100

    added examples and some modifications

--- Components/html/tags.py
+++ Components/html/tags.py
@@ -25,14 +25,46 @@ class Tag:
         return self.create()
     
 class Header(Tag):
-    def __init__(self,type_ , body=None, **kwargs) -> None:
+    def __init__(self,type_=None , body=None, **kwargs) -> None:
         super().__init__( body, **kwargs)
         self.type_ = type_ 
-        if isinstance(self.type_,int):
+        if isinstance(self.type_, int):
             self.name = f"h{self.type_}"
         else:
             self.name = 'header'
         self.create()
 
+def h_tag(type_=None, body=None, **kwargs):
+    param_dict = kwargs
+    tag_obj = Header(type_, body, **param_dict).create()
+    return tag_obj
 
+class Paragraph(Tag):
+    def __init__(self, body=None, **kwargs) -> None:
+        super().__init__(body, **kwargs)
+        self.name = 'p'
+        self.create()
+
+def p_tag(body=None, **kwargs):
+    param_dict = kwargs 
+    tag_obj = Paragraph(body,**param_dict).create()
+    return tag_obj
+
+class Division(Tag):
+    def __init__(self, body=None, **kwargs) -> None:
+        super().__init__(body, **kwargs)
+        self.name = 'div'
+        self.create()
+    
+def div(body=None, **kwargs):
+    param_dict = kwargs
+    tag_obj = Division(body, **param_dict).create()
+    return tag_obj
+
+def make_tag(name:str,**kwargs):
+    if not isinstance(name, str):
+        raise ValueError(f'expected string object got {type(name)}')
+    param_dict = kwargs
+    new_tag = Tag(name=name, **param_dict).create()
+    return new_tag
 
